77528f7545f08c5d5be915c012fdcc5bb0ad3190
Tomczyk <s22472@pjwstk.edu.pl>
2022-01-03 17:14:01 +0100

    All necessary classes and methods (empty for now).

---
+++ src/zad1/Database.java
@@ -0,0 +1,20 @@
+package zad1;
+
+public class Database {
+
+    private String url;
+    private TravelData travelData;
+
+    public Database(String url, TravelData travelData) {
+        this.url = url;
+        this.travelData = travelData;
+    }
+
+    public void create() {
+
+    }
+
+    public void showGui() {
+
+    }
+}
---
+++ src/zad1/TravelData.java
@@ -0,0 +1,16 @@
+package zad1;
+
+import java.io.File;
+import java.util.List;
+
+public class TravelData {
+    private File datafir;
+
+    public TravelData(File datafir) {
+        this.datafir = datafir;
+    }
+
+    public List<String> getOffersDescriptionsList(String loc, String dateFormat) {
+        return null;
+    }
+}
