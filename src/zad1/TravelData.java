360ff6e038fff981bca8a09d14f80088a90cb8d5
Tomczyk <s22472@pjwstk.edu.pl>
2022-01-05 13:20:27 +0100

    Reads all data to list.

--- src/zad1/TravelData.java
+++ src/zad1/TravelData.java
@@ -1,16 +1,51 @@
 package zad1;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.io.IOException;
 
 public class TravelData {
-    private File datafir;
+    private File dataDir;   //"data"
+    private static List<String> output = new ArrayList<>();
 
     public TravelData(File datafir) {
-        this.datafir = datafir;
+        this.dataDir = datafir;
     }
 
     public List<String> getOffersDescriptionsList(String loc, String dateFormat) {
-        return null;
+        readFilesToList(dataDir);
+
+        return output;
+    }
+
+    public void readFilesToList(File dataDir){
+        try {
+            FileVisitor<Path> fileVisitor = new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path visitedFilePath, BasicFileAttributes fileAttributes) throws IOException {
+                    try (BufferedReader in = new BufferedReader(
+                            new InputStreamReader(new FileInputStream(visitedFilePath.toFile()), StandardCharsets.UTF_8))) {
+                        String str;
+                        while ((str = in.readLine()) != null) {
+                            output.add(str);
+                        }
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            };
+
+            Files.walkFileTree(Paths.get(String.valueOf(dataDir)), fileVisitor);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
+
+//https://mkyong.com/java/how-to-read-utf-8-encoded-data-from-a-file-java/
